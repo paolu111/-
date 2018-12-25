7bb34e1b88387fd313ce0db0d261eb9fe77bb1ef
zhangyi1995 <784968240@qq.com>
2018-12-25 16:58:58 +0800

    企业管理初稿

--- apps/seller/index.py
+++ apps/seller/index.py
@@ -1,3 +1,4 @@
+from apps.forms.company import Business_Form
 from apps.models import db
 from apps.models.seller_models import Business
 from apps.seller import seller_log_bp
@@ -27,9 +28,26 @@ def info_com():
         db.session.add(b1)
         db.session.commit()
         return "ok"
+    if request.method == "GET":
+        form = Business_Form(request.form)
+        return render_template("info.html", form=form, title="企业信息")
+    if request.method == "POST":
+        form = Business_Form(request.form)
+        if form.validate():
+            data = request.form
+            b1 = Business()
+            b1.name = data.get("name")
+            b1.email = data.get("email")
+            b1.boss = data.get("boss")
+            b1.tel = data.get("tel")
+            db.session.add(b1)
+            db.session.commit()
+            return "ok"
+        return "off"
 
 @seller_log_bp.route("/show_com/",endpoint="show_com",methods=["GET","POST"])
 def show_comp():
-    buss = Business.query.all()
-    return render_template("show_comp.html", buss=buss)
+    if request.method=="GET":
+        buss = Business.query.all()
+        return render_template("breeding.html", buss=buss)
 
